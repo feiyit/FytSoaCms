7ce7d2b751bb3f02030fbfe22b9e271bd311ada1
fuyu <715515390@qq.com>
2018-04-22 21:43:28 +0800

    基础框架搭建

---
+++ FytErp/Core.Extensions/ConfigServices.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Core.Extensions
+{
+    /// <summary>
+    /// 读取配置文件
+    /// </summary>
+    public class ConfigServices
+    {
+        public static IConfiguration Configuration { get; set; }
+        static ConfigServices()
+        {
+            //ReloadOnChange = true 当appsettings.json被修改时重新加载            
+            Configuration = new ConfigurationBuilder()
+            .Add(new JsonConfigurationSource { Path = "DbSetting.json", ReloadOnChange = true })
+            .Build();
+        }
+    }
+}
---
+++ FytSoa.Extensions/ConfigExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+namespace FytSoa.Extensions
+{
+    /// <summary>
+    /// 读取配置文件信息
+    /// </summary>
+    public class ConfigExtensions
+    {
+        public static IConfiguration Configuration { get; set; }
+        static ConfigExtensions()
+        {
+            //ReloadOnChange = true 当appsettings.json被修改时重新加载            
+            Configuration = new ConfigurationBuilder()
+            .Add(new JsonConfigurationSource { Path = "appsettings.json", ReloadOnChange = true })
+            .Build();
+        }
+    }
+}
