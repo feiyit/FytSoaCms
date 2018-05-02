982bab61c08b90ae2fc73c2ef21da76c585e3338
fuyu <715515390@qq.com>
2018-05-03 00:01:02 +0800

    登录优化，增加layui支持第三方扩展组件

--- FytSoa.Web/wwwroot/themes/js/login.js
+++ FytSoa.Web/wwwroot/themes/js/login.js
@@ -1,21 +1,35 @@
-layui.use(['element', 'jquery', 'form'], function () {
+﻿layui.config({
+    base: '/themes/js/modules/'
+});
+layui.use(['element', 'jquery', 'form', 'common'], function () {
     var form = layui.form,
         layer = layui.layer,
-        $ = layui.jquery;
+        $ = layui.jquery,
+        os = layui.common;
+    $(".layui-btn-danger").click(function () {
+        document.getElementById("forms").reset();
+    });
     form.on('submit(loginsub)', function (data) {
-        console.log(data.field);
-        $.ajax({
-            type: "post",
-            url: "/fytadmin/login?handler=login",
-            data: data.field,
-            success: function (res) {
-                if (res.statusCode === 200) {
+        var btns = $(".layui-btn-normal");
+        btns.html('<i class="layui-icon layui-anim layui-anim-rotate layui-anim-loop"></i>');
+        btns.attr('disabled', 'disabled');        
+        os.ajax('fytadmin/login?handler=login', data.field, function (res) {                    
+            if (res.statusCode === 200) {
+                setTimeout(function () {
                     window.location.href = '/fytadmin/index';
-                } else {
-                    alert(res.message);
-                }
+                },1000)                
+            } else {
+                $(".login-tip span").html(res.message);
+                $(".login-tip").animate({ 'height': '30px' });
+                setTimeout(function () {
+                    $(".login-tip").animate({ 'height': 0 });
+                }, 2500);
             }
-        })
+            btns.attr('disabled', false);    
+            setTimeout(function () {
+                btns.html('登录');
+            },1000);
+        })       
         return false;
     });
     $(window).resize(
---
+++ FytSoa.Web/wwwroot/themes/js/modules/common.js
@@ -0,0 +1,58 @@
+﻿layui.define(['layer','toastr'], function (exports) {
+    "use strict";
+
+    var $ = layui.jquery,
+        layer = layui.layer,
+        toastr=layui.toastr;
+    var tool = {
+        error: function (msg) {
+            toastr.error(msg);
+        },
+        warning: function (msg) {
+            toastr.warning(msg);
+        },
+        success: function (msg) {
+            toastr.success(msg);
+        },
+        ajax: function (url, options, callFun) {
+            var httpUrl = "/";
+            $.ajax(httpUrl + url, {
+                data: options,
+                async: false,
+                dataType: 'json', //服务器返回json格式数据
+                type: 'post', //HTTP请求类型
+                timeout: 50 * 1000, //超时时间设置为50秒；
+                success: function (data) {
+                    callFun(data);
+                },
+                error: function (xhr, type, errorThrown) {
+                    if (type === 'timeout') {
+                        tool.error('连接超时，请稍后重试！');
+                    } else if (type === 'error') {
+                        tool.error('连接异常，请稍后重试！');
+                    }
+                }
+            });
+        },
+        Open: function (title, url, width, height, fun) {
+            layer.open({
+                type: 2,
+                title: title,
+                shadeClose: true,
+                shade: 0.3,
+                maxmin: false, //开启最大化最小化按钮
+                area: [width, height],
+                content: url,
+                zIndex: "10",
+                end: function () {
+                    if (fun) fun();
+                }
+            });
+        },
+        closeOpen: function () {
+            layer.closeAll();      
+        }, 
+    };
+    exports('common', tool);
+});
+
