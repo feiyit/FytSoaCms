acfb3da1c03746fd921240886cb4631ae550b865
fuyu <715515390@qq.com>
2018-06-03 12:58:42 +0800

    增加登录验证

--- FytSoa.Web/Pages/FytAdmin/Sys/CodesModify.cshtml.cs
+++ FytSoa.Web/Pages/FytAdmin/Sys/CodesModify.cshtml.cs
@@ -5,11 +5,13 @@ using System.Threading.Tasks;
 using FytSoa.Core.Model.Sys;
 using FytSoa.Service.DtoModel;
 using FytSoa.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FytSoa.Web.Pages.FytAdmin.Sys
 {
+    [Authorize]
     public class CodesModifyModel : PageModel
     {
         private readonly ISysCodeTypeService _sysCodeTypeService;
--- FytSoa.Web/Pages/FytAdmin/Sys/MenuModify.cshtml.cs
+++ FytSoa.Web/Pages/FytAdmin/Sys/MenuModify.cshtml.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using FytSoa.Core.Model.Sys;
 using FytSoa.Service.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace FytSoa.Web.Pages.FytAdmin.Sys
-{
+{ 
+    [Authorize]
     public class MenuModifyModel : PageModel
     {
         private readonly ISysMenuService _sysMenuService;
